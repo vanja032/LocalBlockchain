11c470bb2df5d01f43031fba88e93011e3ae9e95
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2023-03-07 18:36:40 +0100

    Update blockchain_setup_commands.sh

--- local_chain/blockchain_setup_commands.sh
+++ local_chain/blockchain_setup_commands.sh
@@ -1,5 +1,7 @@
-cline create wallet --file def.txt
-Key=PW5JLjYb3Pim4s8exJy1kTxgYs7kSUchj2YmjS4qYNmRgngVq5rHb
+cleos create wallet --file def.txt
+Key=<wallet default unlock key>
+
+cleos wallet unlock --password <wallet default unlock key>
 
 ./genesis_start.sh 
 
@@ -9,16 +11,16 @@ curl --request POST \
     --url http://127.0.0.1:8888/v1/producer/schedule_protocol_feature_activations \
     -d '{"protocol_features_to_activate": ["0ec7e080177b2c02b278d5088611686b49d739925a92d9bfcacd7fc6b74053bd"]}'
 
-cline set contract inery ./inery.boot/
+cleos set contract eosio ./eosio.boot/
 
 ./activate_features.sh 
 
-cline set contract inery ./inery.system inery.system.wasm inery.system.abi
+cleos set contract eosio ./eosio.system eosio.system.wasm eosio.system.abi
 
-cline set contract inery.token ./inery.token inery.token.wasm inery.token.abi
+cleos set contract eosio.token ./eosio.token eosio.token.wasm eosio.token.abi
 
-cline push action inery.token create '[ "inery", "800000000.0000 INR" ]' -p inery.token@active
+cleos push action eosio.token create '[ "eosio", "800000000.0000 EOS" ]' -p eosio.token@active
 
-cline push action inery.token issue '[ "inery", "450000000.0000 INR", "Issuing tokens for inery account" ]' -p inery@active
+cleos push action eosio.token issue '[ "eosio", "450000000.0000 EOS", "Issuing tokens for eosio account" ]' -p eosio@active
 
-cline push action inery init '["0", "4,INR"]' -p inery@active
+cleos push action eosio init '["0", "4,EOS"]' -p eosio@active
